38b5e2d4ce0cd92f725c5e6a923e11553d7fe3e3
Dylan Nelson <Dylan.Nelson@nytimes.com>
2017-09-18 20:06:27 -0400

    Remove unused code; add coverage to npm test

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,3 @@
-if (typeof global !== 'undefined') {
-  const fetch = require('node-fetch').default;
-  global.fetch = fetch;
-}
-
-const mockFetch = require('fetch-mock');
-const resourceJson = require('./test/mock-api/json/resource.json');
-
-mockFetch.get('*', resourceJson);
-
 module.exports = {
   coveragePathIgnorePatterns: [
     '/node_modules/',
